997e7484495f95ddbc2012a7992a1aff6de3761f
Myat Thura Soe <89846206+MyatThuraSoe@users.noreply.github.com>
2022-08-12 14:13:32 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port =  process.env.PORT || 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use('/img', express.static(path.resolve(__dirname, "public/static")));
+// routes
+app.use('/', require('./routes/profile')());
+
+// start server
+const server = app.listen(port);
+console.log('Express started. Listening on %s', port);
